53edf7dd6c9ac16c1d6c4e3bd4e32b7adb1b7875
duah-amankwa <vamankwa18@gmail.com>
2023-02-18 10:38:20 -0800

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - Entry point
  *
  * Return : 0 (Sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
