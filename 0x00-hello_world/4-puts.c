dd3323628a940728bd71fd0437b0c7fc31ff6169
duah-amankwa <vamankwa18@gmail.com>
2023-02-18 10:25:35 -0800

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *
+ * Return : Always 0 (meaning Success)
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a muiltingual puzzle");
+	return (0);
+}
