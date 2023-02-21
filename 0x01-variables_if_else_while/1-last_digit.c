c6c179bc0c7caf683a24fdf42f8701daff2f50a2
duah-amankwa <vamankwa18@gmail.com>
2023-02-21 07:50:25 -0800

    Last Number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,10 +4,8 @@
 /* more headers goes here*/
 /**
  * betty style doc for function main goes there
- * 
  * main - To determin if last digit of a random number is 
  * greater than or less than 5, or zero
- *
  * Return : 0 on success
  */
 int main(void)
@@ -23,7 +21,6 @@ int main(void)
 	else if (n % 10 == 0)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else 
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
-
