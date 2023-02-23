78e92c73c90574885f6755dba7073c16b6925672
duah-amankwa <vamankwa18@gmail.com>
2023-02-23 09:55:16 -0800

    Print alphabets except 'e' and 'p'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry block
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char l;
+
+	for(l = 'a'; l <= 'z'; l++)
+	{
+		if(l != 'q' && l != 'e')
+			putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
