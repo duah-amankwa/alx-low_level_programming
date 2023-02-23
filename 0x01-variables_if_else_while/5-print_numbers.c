9c0ce45e54f3ffb2bd3cfd44512ac41a5363c3f7
duah-amankwa <vamankwa18@gmail.com>
2023-02-23 10:12:24 -0800

    Numbers or digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return : Always 0 
+ */
+int main (void)
+{
+	int singleDigit;
+
+	for (singleDigit = 0; singleDigit < 10; singleDigit++)
+		printf("%d",singleDigit);
+		printf("\n");
+		
+	return (0);
+}
