75c6261f38ce0c99349e852bbb07d04148f250df
duah-amankwa <vamankwa18@gmail.com>
2023-02-23 15:45:48 -0800

    Single digits without char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Single digits of base 10
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+		{
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
