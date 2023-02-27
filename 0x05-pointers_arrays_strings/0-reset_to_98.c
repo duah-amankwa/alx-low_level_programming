1c74e7e39f38b8bdb9a733de24733e4f5b8597bc
duah-amankwa <vamankwa18@gmail.com>
2023-02-27 10:53:52 -0800

    ...A parameter and updates to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,8 +4,9 @@
 * reset_to_98 - a function that takes a pointer to an integer as
 * parameter and updates the value to 98
 * @n: the int decleared variable name
+*
 * Return: int
-* */
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
