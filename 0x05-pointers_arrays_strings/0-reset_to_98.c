aa1746a427f43fe73eb7db376b5ff66c9a1dc203
duah-amankwa <vamankwa18@gmail.com>
2023-02-27 10:38:24 -0800

    ...A parameter and updates tp 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - a function that takes a pointer and updates to 98
- *
- * Return : Always 0.
- */
+* reset_to_98 -  a function that takes a pointer to an integer as 
+* parameter and updates the value to 98.
+* 
+* Return: int
+**/
 
 void reset_to_98(int *n)
 {
