d30e44afa6e48af24add72918580d46e0547da6b
duah-amankwa <vamankwa18@gmail.com>
2023-02-27 11:02:12 -0800

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - Prints a string in reverse
+* @s: the nme of the variable char
+*
+* Return: string
+**/
+void print_rev(char *s)
+{
+int i;
+char c;
+
+for (i = 0; s[i] != 0; i++)
+{
+}
+for (i = i - 1; i >= 0; i--)
+{
+c = s[i];
+_putchar(c);
+}
+_putchar('\n');
+}
