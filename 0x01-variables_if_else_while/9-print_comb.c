99c191391af3654d2c43558f13e1814329f83fa8
duah-amankwa <vamankwa18@gmail.com>
2023-02-28 08:33:26 -0800

    Print all possible single digit combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i;
 	/*Printing all possible combinations of single digit numbers */
-	for(i =0; i < 10 ; i++)
+	for (i = 0; i < 10 ; i++)
     {
-    	putchar((i% 10) + '0');
+    	putchar((i % 10) + '0');
 		if (i == 9)
-			continue;
+		continue;
 	putchar(',');
 	putchar(' ');
     }
 	putchar('\n');
-	return(0);
+	return (0);
 }
