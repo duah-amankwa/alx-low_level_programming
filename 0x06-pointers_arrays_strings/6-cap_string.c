d300dda1613b26b5d69700e46bdf518325901ab7
duah-amankwa <vamankwa18@gmail.com>
2023-03-02 14:30:35 -0800

    Capitalize all words of a string2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,42 @@
-#include "main.h"
+int check_seperators(char c);
 
 /**
-* *cap_string - a function that capitalizes all words of a string
-* @str: string
-* Return: string
-**/
-
-char *cap_string(char *str)
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
 {
-	int i = 1;
+	int i = 0;
 
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] = str[0] - 32;
-
-	while (str[i] != '\0')
+	while (s[i])
 	{
-		if ((str[i - 1] == ' ' || str[i - 1] == '\t'
-		     || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
-		     || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
-		     || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
-		     || str[i - 1] == '{' || str[i - 1] == '}')
-		     && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-		}
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
 		i++;
 	}
-	return (str);
+
+	return (s);
+}
+
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }
