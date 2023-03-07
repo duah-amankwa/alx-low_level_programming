da81b841a878bf733428bed279002a19038754c9
duah-amankwa <vamankwa18@gmail.com>
2023-03-07 13:43:32 -0800

    Check for uppercase character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,28 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
+/* This a header file containing declarations
+ * for prototype functions defined in the 0x02-functions_nested_loops
+ * tasks and directory
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ */
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
 int _isupper(int c);
 int _isdigit(int c);
-void print_numbers(void);
-void print_triangle(int size);
-void print_square(int size);
-void print_diagonal(int n);
-void print_line(int n);
-void more_numbers(void);
-void print_most_numbers(void);
 int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void print_number(int n);
-int b10_pow(int idx);
 #endif
