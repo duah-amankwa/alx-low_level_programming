b47f006183047a41a55c88d3f8638cefa722300e
duah-amankwa <vamankwa18@gmail.com>
2023-03-20 15:00:20 -0700

    Header fiel that deines a macro namaed SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,13 +1,4 @@
 #ifndef SIZE_H
 #define SIZE_H
-
-/*
- * File: 0-object_like_macro.h
- * Description: Header file that defines a macro named
- *              SIZE as an abbreviation for the token 1024.
- *		Headerfile guarded to avoid problem of double inclusion.
- */
-
 #define SIZE 1024
-
 #endif
