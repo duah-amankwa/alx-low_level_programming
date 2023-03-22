f181845a01c57e863ab7f81e3a1cafa85f4a6ec9
duah-amankwa <vamankwa18@gmail.com>
2023-03-22 02:52:26 -0700

    Lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,15 @@
 #include "mian.h"
 
 /**
- * _islower - chekcs if a character is a lowercase
+ *_islower- Return 1 if letter is lowercase, 0 if not.
  * @c: the character to be checked
  *
  * Return : 1 if character is a lowercase, 0 if not
  */
-
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
+	else
 		return (0);
 }
